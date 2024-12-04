854412b7e354c1ba53dc1c16834aa503642161fd
Snehal Suman <snehals0203@gmail.com>
2024-12-04 22:35:33 +0530

    Initial commit

---
+++ src/main/java/com/snehal/SpringJdbcTemplate123/SpringJdbc123Application.java
@@ -0,0 +1,13 @@
+package com.snehal.SpringJdbcTemplate123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbc123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbc123Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/snehal/SpringJdbcTemplate123/SpringJdbc123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snehal.SpringJdbcTemplate123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbc123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
